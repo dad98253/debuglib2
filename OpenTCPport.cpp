3870982a31058d2253ef41c06005d6b2cbba5feb
John Kuras <w7og@yahoo.com>
2020-11-21 14:22:40 -0800

    enclosed previous change in ifdef DEBUG blocks

--- OpenTCPport.cpp
+++ OpenTCPport.cpp
@@ -55,7 +55,7 @@ int OpenTCPport(void) {
 	} else {
 		server_name = lpDebugServerName;
 	}
-
+#ifdef DEBUG
 	if ( debugCheckflags( DEBUGOPENTCPPORT ) ) {
 		if ( iDebugServerPort != 0 ) {
 			fprintf(stderr,"opening server %s for port %i\n",server_name,iDebugServerPort);
@@ -63,7 +63,7 @@ int OpenTCPport(void) {
 			fprintf(stderr,"opening server %s for port %i\n",server_name,port);
 		}
 	}
-
+#endif	// DEBUG
 #ifdef WINDOZE
 	if (WSAStartup(0x202,&wsaData) == SOCKET_ERROR) {
 		fprintf(stderr,"WSAStartup failed with error %d\n",WSAGetLastError());
@@ -83,12 +83,14 @@ int OpenTCPport(void) {
 	}
 	if (hp == NULL ) {
 #ifdef WINDOZE
-
+#ifdef DEBUG
 		if ( debugCheckflags( DEBUGOPENTCPPORT ) ) fprintf(stderr,"Client: Cannot resolve address [%s]: Error %d\n",
 			server_name,WSAGetLastError());
+#endif	// DEBUG
 		WSACleanup();
 		return 0;
 #else	// WINDOZE
+#ifdef DEBUG
 		if ( debugCheckflags( DEBUGOPENTCPPORT ) ) {
 			int errsv = errno;
 			int herrsv = h_errno;
@@ -97,7 +99,7 @@ int OpenTCPport(void) {
 			fprintf(stderr,"%s\n", hstrerror(h_errno));
 			if (h_errno == HOST_NOT_FOUND) fprintf(stderr,"Note: on linux, ip addresses must have valid RDNS entries\n");
 		}
-
+#endif	// DEBUG
 #endif	// WINDOZE
 //		return 0;	////////////   ???? why is this commented out on linux??
 	}
@@ -140,7 +142,7 @@ int OpenTCPport(void) {
 	//    ( LocalIPAddress, LocalPort, RemoteIP, RemotePort) mapping.
 	//    This enables us to use send() and recv() on datagram sockets,
 	//    instead of recvfrom() and sendto()
-
+#ifdef DEBUG
 	if ( debugCheckflags( DEBUGOPENTCPPORT ) ) {
 		if ( hp != NULL ) {
 			fprintf(stderr,"Client connecting to: %s\n",hp->h_name);
@@ -148,6 +150,7 @@ int OpenTCPport(void) {
 			fprintf(stderr,"Client connecting to: %s\n",server_name);
 		}
 	}
+#endif	// DEBUG
 	if (connect(conn_socket,(struct sockaddr*)&server,sizeof(server))
 		== SOCKET_ERROR) {
 #ifdef WINDOZE
